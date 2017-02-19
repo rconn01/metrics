66fd90942461c72023137b6655032d8801d3a101
Raymond Conn <Raymond.conn90@gmail.com>
2017-02-19 11:41:22 -0500

    change the DefaultObjectNameFactory so that it makes use of tags and prevents collisions with MetricNames

--- metrics-core/src/main/java/io/dropwizard/metrics/DefaultObjectNameFactory.java
+++ metrics-core/src/main/java/io/dropwizard/metrics/DefaultObjectNameFactory.java
@@ -7,16 +7,33 @@ public class DefaultObjectNameFactory implements ObjectNameFactory {
 
 	@Override
 	public ObjectName createName(String type, String domain, MetricName metricName) throws MalformedObjectNameException {
-	    String name = metricName.getKey();
-		try {
-			ObjectName objectName = new ObjectName(domain, "name", name);
-			if (objectName.isPattern()) {
-				objectName = new ObjectName(domain, "name", ObjectName.quote(name));
-			}
-			return objectName;
-		} catch (MalformedObjectNameException e) {
-			return new ObjectName(domain, "name", ObjectName.quote(name));
+		StringBuilder nameBuilder = new StringBuilder(domain);
+		nameBuilder.append(":name=");
+		nameBuilder.append(formatProperty(metricName.getKey()));
+		metricName.getTags().forEach((k,v)-> {
+			nameBuilder.append(",");
+			nameBuilder.append(formatProperty(k));
+			nameBuilder.append("=");
+			nameBuilder.append(formatProperty(v));
+		});
+		String objNameStr = nameBuilder.toString();
+		ObjectName objectName = new ObjectName(objNameStr);
+		return objectName;
+	}
+
+
+	/**
+	 * Checks to see if the given string is patterned by checking to
+	 * see if it contains a {@code *} or {@code ?} if so it will quote them
+	 * @param property the property to check for patterned
+	 * @return a valid string to add to the ObjectName
+	 */
+	private String formatProperty(String property) {
+		if (property.contains("*") || property.contains("?")) {
+			property = ObjectName.quote(property);
 		}
+
+		return property;
 	}
 
 }
--- metrics-core/src/test/java/io/dropwizard/metrics/DefaultObjectNameFactoryTest.java
+++ metrics-core/src/test/java/io/dropwizard/metrics/DefaultObjectNameFactoryTest.java
@@ -2,27 +2,66 @@ package io.dropwizard.metrics;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import io.dropwizard.metrics.DefaultObjectNameFactory;
-import io.dropwizard.metrics.MetricName;
 
 public class DefaultObjectNameFactoryTest {
+	
+	private ObjectNameFactory factory;
+	private Map<String,String> tags;
+	
+	@Before
+	public void setup() {
+		factory = new DefaultObjectNameFactory();
+		tags = new HashMap<>();
+		tags.put("k1", "v1");
+		tags.put("k3", "v3");
+		
+	}
 
 	@Test
 	public void createsObjectNameWithDomainInInput() throws MalformedObjectNameException {
-		DefaultObjectNameFactory f = new DefaultObjectNameFactory();
-		ObjectName on = f.createName("type", "com.domain", MetricName.build("something.with.dots"));
+		ObjectName on = factory.createName("type", "com.domain", MetricName.build("something.with.dots"));
 		assertThat(on.getDomain()).isEqualTo("com.domain");
 	}
 
 	@Test
 	public void createsObjectNameWithNameAsKeyPropertyName() throws MalformedObjectNameException {
-		DefaultObjectNameFactory f = new DefaultObjectNameFactory();
-		ObjectName on = f.createName("type", "com.domain", MetricName.build("something.with.dots"));
+		ObjectName on = factory.createName("type", "com.domain", MetricName.build("something.with.dots"));
 		assertThat(on.getKeyProperty("name")).isEqualTo("something.with.dots");
+	}
+	
+	@Test
+	public void createsObjectWithTags() throws MalformedObjectNameException {		
+		MetricName name = new MetricName("a.name",tags);
+		ObjectName on = factory.createName("meter", "metrics", name);
+		
+		assertThat(on.getDomain()).isEqualTo("metrics");
+		assertThat(on.getKeyProperty("name")).isEqualTo("a.name");
+		assertThat(on.getKeyProperty("k1")).isEqualTo("v1");
+		assertThat(on.getKeyProperty("k3")).isEqualTo("v3");		
+	}
+	
+	@Test
+	public void createsQuotedObjectNames() throws MalformedObjectNameException {
+		tags.put("test", "*");
+		MetricName name = new MetricName("a.name",tags);
+		ObjectName on = factory.createName("meter", "metrics", name);
+		
+		assertThat(on.isPattern()).isFalse();
+		assertThat(on.getKeyProperty("test")).isEqualTo(ObjectName.quote("*"));
+		
+		name = new MetricName("a.name.?");
+		on = factory.createName("meter", "metrics", name);
+		
+		assertThat(on.isPattern()).isFalse();
+		assertThat(ObjectName.unquote(on.getKeyProperty("name"))).isEqualTo("a.name.?");
 	}
 }
